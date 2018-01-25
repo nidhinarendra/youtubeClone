c428a770e2033afcc667b49ed6f3597a26a9fb53
Nidhi Narendra <nidhi.narendra1@gmail.com>
2018-01-24 23:37:25 -0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  devtool: 'source-map',
+
+  entry: ['./src/index.js'],
   output: {
     path: __dirname,
     publicPath: '/',
     filename: 'bundle.js'
   },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
       }
-    }]
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  ],
+
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
